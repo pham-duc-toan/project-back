52a25f75e627db0053401aeb36ca38d92dd7672a
Phạm Đức Toàn <phamductoan220704@gmail.com>
2023-09-14 18:55:05 +0700

    lan 2

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports.connect = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/product-manager"); 
+    await mongoose.connect("mongodb://127.0.0.1:27017/product-manager"); 
     console.log("Connect Success!");
   } catch (error) {
     console.log("connect error");
--- controller/clients/product.controller.js
+++ controller/clients/product.controller.js
@@ -1,5 +1,14 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model")
+
+module.exports.index = async (req, res) => {
+  const products = await Product.find({});
+  const productsNew = await products.map (item =>{
+    item.priceNew= Math.round(item.price*(100 - item.discountPercentage)/100);
+    return item;
+  })
+  console.log(products);
     res.render("clients/page/products/index", {
-      pageTitle: "Danh sách sản phẩm"
+      pageTitle: "Danh sách sản phẩm",
+      products: products
     });
   }
