fb552ecc71cc929323aad0dfaaebd3521885be3d
Phạm Đức Toàn <phamductoan220704@gmail.com>
2023-09-15 16:19:21 +0700

    bo loc trang thai standard

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports.connect = async () => {
   try {
-    await mongoose.connect("process.env.mongourl"); 
+    await mongoose.connect(process.env.mongourl); 
     console.log("Connect Success!");
   } catch (error) {
     console.log(error);
