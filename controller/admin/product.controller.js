44cc8663f5b0eff40dcdbf7190e5332ea3ecebbd
Phạm Đức Toàn <phamductoan220704@gmail.com>
2023-09-17 11:05:48 +0700

    toi uu va tao tinh nang phan trang

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
 // // [GET] /admin/products
 module.exports.index = async (req, res) => {
 
@@ -14,16 +15,28 @@ let objectSearch = searchHelper(req.query);
   if(req.query.status) {
     find.status = req.query.status;
   }
-  if(objectSearch.regex)
-  find.title = objectSearch.regex;
-  const products = await Product.find(find);
+  if(objectSearch.regex){
+    find.title = objectSearch.regex;
+  }
+    // Pagination
+    let initPagination = {
+      currentPage: 1,
+      limitItems: 4
+    };
+    const countProducts = await Product.count(find);
+    const objectPagination = paginationHelper(initPagination, req.query, countProducts);
+    // End Pagination
+  
+    const products = await Product.find(find)
+      .limit(objectPagination.limitItems)
+      .skip(objectPagination.skip);
+  
   
   res.render("admin/page/products/index", {
     pageTitle: "Danh sách sản phẩm",
-    products: products,
-    
-    filterStatus: filterStatus,
-   
-    keyword: objectSearch.keyword
+    products: products,    
+    filterStatus: filterStatus,   
+    keyword: objectSearch.keyword,
+    pagination: objectPagination
   });
 }
