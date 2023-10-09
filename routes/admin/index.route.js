ba4f88d1ef040f594cb3d65ddbccf8d930480696
Phạm Đức Toàn <phamductoan220704@gmail.com>
2023-10-09 17:31:52 +0700

    xong trang đăng nhập

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -4,6 +4,7 @@ const productRoutes = require("./product.route");
 const productCategoryRoutes = require("./product-category.route");
 const accountRoutes = require("./account.route");
 const roleRoutes = require("./role.route");
+const authRoutes = require("./auth.route");
 module.exports = (app) => {
   const PATH_ADMIN = "/" + systemConfig.prefixAdmin;
   
@@ -12,4 +13,5 @@ module.exports = (app) => {
   app.use(PATH_ADMIN + "/products-category", productCategoryRoutes);
   app.use(PATH_ADMIN + "/roles", roleRoutes);
   app.use(PATH_ADMIN + "/accounts", accountRoutes);
+  app.use(PATH_ADMIN + "/auth", authRoutes);
 }
