f235f6166bc68efb30796ad1c4a777750870c5ea
Phạm Đức Toàn <phamductoan220704@gmail.com>
2023-10-18 15:26:48 +0700

    thêm sản phẩm vào giỏ hàng trong database

--- routes/clients/index.route.js
+++ routes/clients/index.route.js
@@ -2,11 +2,15 @@ const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
 const searchRoutes = require("./search.route");
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
-
+const cartMiddleware = require("../../middlewares/client/cart.middleware");
+const cartRoutes = require("./cart.route");
 module.exports = (app) => {
   app.use(categoryMiddleware.category);
+  app.use(cartMiddleware.cartId);
+
   app.use("/", homeRoutes);
   console.log("route");
+  app.use("/cart", cartRoutes);
   app.use("/products", productRoutes);
   app.use("/search", searchRoutes);
 }
