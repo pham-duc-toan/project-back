be40b4aaacbf16e58c9aed484e96365f253ac8a8
Phạm Đức Toàn <phamductoan220704@gmail.com>
2023-10-18 14:56:50 +0700

    thêm trang tìm kiếm sản phẩm client

--- routes/clients/index.route.js
+++ routes/clients/index.route.js
@@ -1,5 +1,6 @@
 const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
+const searchRoutes = require("./search.route");
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 
 module.exports = (app) => {
@@ -7,4 +8,5 @@ module.exports = (app) => {
   app.use("/", homeRoutes);
   console.log("route");
   app.use("/products", productRoutes);
+  app.use("/search", searchRoutes);
 }
---
+++ routes/clients/search.route.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router  = express.Router();
+
+const controller = require("../../controller/clients/search.controller");
+
+router.get("/", controller.index);
+
+module.exports = router;
