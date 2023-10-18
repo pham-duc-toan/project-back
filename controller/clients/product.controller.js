cd7da3e7c438a8a155effabeaa200056ae568265
Phạm Đức Toàn <phamductoan220704@gmail.com>
2023-10-18 14:37:13 +0700

    trang danh sách sản phẩm bên client, lấy cả danh mục con và sửa lại link cho chi tiết sản phẩm

--- controller/clients/product.controller.js
+++ controller/clients/product.controller.js
@@ -1,4 +1,6 @@
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
+
 const productsHelper = require("../../helpers/products");
 // [GET] /products
 
@@ -15,6 +17,29 @@ module.exports.index = async (req, res) => {
     });
   }
 
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+  const slugCategory = req.params.slugCategory;
+
+  const category = await ProductCategory.findOne({
+    slug: slugCategory,
+    deleted: false,
+    status: "active"
+  });
+
+  const products = await Product.find({
+    product_category_id: category.id,
+    status: "active",
+    deleted: false
+  }).sort({ position: "desc" });
+
+  const newProducts = productsHelper.priceNewProducts(products);
+
+  res.render("client/pages/products/index", {
+    pageTitle: "Danh sách sản phẩm",
+    products: newProducts
+  });
+}
 
 // [GET] /products/detail/:slug
 module.exports.detail = async (req, res) => {
