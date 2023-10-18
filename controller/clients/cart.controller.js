12837f839661d965c33893ce3658bb57b0437fe8
Phạm Đức Toàn <phamductoan220704@gmail.com>
2023-10-18 17:31:55 +0700

    thêm chức năng giỏ hàng

--- controller/clients/cart.controller.js
+++ controller/clients/cart.controller.js
@@ -1,5 +1,39 @@
 const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
 
+const productsHelper = require("../../helpers/products");
+
+// [GET] /cart/
+module.exports.index = async (req, res) => {
+  const cartId = req.cookies.cartId;
+
+  const cart = await Cart.findOne({
+    _id: cartId
+  });
+
+  if(cart.products.length > 0) {
+    for(const item of cart.products) {
+      const productId = item.product_id;
+
+      const productInfo = await Product.findOne({
+        _id: productId
+      });
+
+      productInfo.priceNew = productsHelper.priceNewProduct(productInfo);
+
+      item.productInfo = productInfo;
+
+      item.totalPrice = item.quantity * productInfo.priceNew;
+    }
+  }
+
+  cart.totalPrice = cart.products.reduce((sum, item) => sum + item.totalPrice, 0);
+
+  res.render("clients/page/cart/index", {
+    pageTitle: "Giỏ hảng",
+    cartDetail: cart
+  });
+}
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
   const cartId = req.cookies.cartId;
@@ -43,6 +77,42 @@ module.exports.addPost = async (req, res) => {
   }
 
   req.flash("success", "Thêm sản phẩm vào giỏ hàng thành công!");
+
+  res.redirect("back");
+}
+// [GET] /cart/delete/:productId
+module.exports.delete = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const productId = req.params.productId;
+
+  console.log(productId);
+  await Cart.updateOne({
+    _id: cartId
+  }, {
+    "$pull": { products: { "product_id": productId } }
+  });
+
+  req.flash("success", "Đã xóa sản phẩm khỏi giỏ hàng!");
+
+  res.redirect("back");
+}
+// [GET] /cart/update/:productId/:quantity
+module.exports.update = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const productId = req.params.productId;
+  const quantity = req.params.quantity;
+
+  await Cart.updateOne(
+    {
+      _id: cartId,
+      'products.product_id': productId
+    },
+    {
+      'products.$.quantity': quantity
+    }
+  );
+
+  req.flash("success", "Đã cập nhật số lượng!");
 
   res.redirect("back");
 }
--- routes/clients/cart.route.js
+++ routes/clients/cart.route.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router  = express.Router();
 
 const controller = require("../../controller/clients/cart.controller");
-
+router.get("/", controller.index);
+router.get("/delete/:productId", controller.delete);
+router.get("/update/:productId/:quantity", controller.update);
 router.post("/add/:productId", controller.addPost);
 
 module.exports = router;
