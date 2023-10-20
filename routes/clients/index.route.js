b0abc23d88db60a2ba2c00ddc3cd827b8654728f
Phạm Đức Toàn <phamductoan220704@gmail.com>
2023-10-20 08:21:42 +0700

    dang ky tao khoan

--- routes/clients/index.route.js
+++ routes/clients/index.route.js
@@ -5,6 +5,7 @@ const categoryMiddleware = require("../../middlewares/client/category.middleware
 const cartMiddleware = require("../../middlewares/client/cart.middleware");
 const cartRoutes = require("./cart.route");
 const checkoutRoutes = require("./checkout.route");
+const userRoutes = require("./user.route");
 module.exports = (app) => {
   app.use(categoryMiddleware.category);
   app.use(cartMiddleware.cartId);
@@ -15,4 +16,5 @@ module.exports = (app) => {
   app.use("/checkout", checkoutRoutes);
   app.use("/products", productRoutes);
   app.use("/search", searchRoutes);
+  app.use("/user", userRoutes);
 }
