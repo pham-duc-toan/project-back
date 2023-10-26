2d4e1f2d9297c8383d6eaece6a9437e3d1cba585
Phạm Đức Toàn <phamductoan220704@gmail.com>
2023-10-26 15:18:27 +0700

    quên mật khẩu full

--- controller/clients/user.controller.js
+++ controller/clients/user.controller.js
@@ -4,7 +4,7 @@ const User = require("../../models/user.model");
 const ForgotPassword = require("../../models/forgot-password.model");
 
 const generateHelper = require("../../helpers/generate");
-
+const sendMailHelper = require("../../helpers/sendMail");
 // [GET] /user/register
 module.exports.register = async (req, res) => {
   res.render("clients/page/user/register", {
@@ -113,7 +113,12 @@ module.exports.forgotPasswordPost = async (req, res) => {
   await forgotPassword.save();
 
   // Việc 2: Gửi mã OTP qua email của user
+  const subject = `Mã OTP xác minh lấy lại mật khẩu`;
+  const html = `
+    Mã OTP xác minh lấy lại mật khẩu là <b>${otp}</b>. Thời hạn sử dụng là 3 phút. Lưu ý không được để lộ mã OTP.
+  `;
 
+  sendMailHelper.sendMail(email, subject, html);
   res.redirect(`/user/password/otp?email=${email}`);
 };
 
@@ -156,3 +161,23 @@ module.exports.otpPasswordPost = async (req, res) => {
 
   res.redirect("/user/password/reset");
 }
+// [GET] /user/password/reset
+module.exports.resetPassword = async (req, res) => {
+  res.render("clients/page/user/reset-password", {
+    pageTitle: "Đổi mật khẩu",
+  });
+};
+
+// [POST] /user/password/reset
+module.exports.resetPasswordPost = async (req, res) => {
+  const password = req.body.password;
+  const tokenUser = req.cookies.tokenUser;
+
+  await User.updateOne({
+    tokenUser: tokenUser
+  }, {
+    password: md5(password)
+  });
+
+  res.redirect("/");
+};
