88e1f8428d3645a8fc0912c0e20b5cc2cd27b3e1
Phạm Đức Toàn <phamductoan220704@gmail.com>
2024-07-26 22:32:54 +0700

    to be continue

--- controller/admin/user.controller.js
+++ controller/admin/user.controller.js
@@ -9,3 +9,49 @@ module.exports.index = async (req, res) => {
     records: records,
   });
 };
+// [PATCH] /admin/users/change-status/:status/:id
+module.exports.changeStatus = async (req, res) => {
+  try {
+    await User.updateOne(
+      { _id: req.params.id },
+      {
+        status: req.params.status,
+      }
+    );
+    res.redirect("back");
+  } catch (error) {
+    res.redirect("back");
+    console.log(error);
+  }
+};
+// [DELETE] /admin/users/delete/:id
+module.exports.deleteItem = async (req, res) => {
+  try {
+    await User.deleteOne({ _id: req.params.id });
+    res.redirect("back");
+  } catch (error) {
+    res.redirect("back");
+    console.log(error);
+  }
+};
+// [GET] /admin/users/detail/:id
+module.exports.detail = async (req, res) => {
+  try {
+    const item = await User.findOne({
+      _id: req.params.id,
+    });
+    for (const friend of item.listFriend) {
+      const friendInfo = await User.findOne({
+        _id: friend.friend_id,
+      }).select("fullName avatar id");
+      friend.friendInfo = friendInfo;
+    }
+    res.render("admin/page/user/detail", {
+      pageTitle: "Chi tiết khách hàng",
+      item: item,
+    });
+  } catch (error) {
+    res.redirect("back");
+    console.log(error);
+  }
+};
--- routes/admin/user.router.js
+++ routes/admin/user.router.js
@@ -3,5 +3,7 @@ const router = express.Router();
 
 const controller = require("../../controller/admin/user.controller");
 router.get("/", controller.index);
-
+router.get("/detail/:id", controller.detail);
+router.delete("/delete/:id", controller.deleteItem);
+router.patch("/change-status/:status/:id", controller.changeStatus);
 module.exports = router;
